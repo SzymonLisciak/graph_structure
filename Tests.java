14a127da469fd2c38e2fdb4a1df6d5d2707a505e
SzymonLisciak <155400528+SzymonLisciak@users.noreply.github.com>
2024-04-25 13:40:36 +0200

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class Tests {
 
     Graph graph;
@@ -8,18 +10,52 @@ public class Tests {
     @BeforeEach
     public void setUp() {
         graph = new Graph();
-        graph.addNode(((int) 'a'));
+        // Dodawanie wierzchołków
+        graph.addNode(1);
         graph.addNode(2);
         graph.addNode(3);
         graph.addNode(4);
         graph.addNode(5);
-        graph.addEdge(1, 2, 3);
-        graph.addEdge(2, 3, 4);
+
+        // Dodawanie krawędzi
+        graph.addEdge(1, 2, 10);
+        graph.addEdge(1, 3, 15);
+        graph.addEdge(1, 4, 20);
+        graph.addEdge(2, 3, 35);
+        graph.addEdge(2, 4, 25);
+        graph.addEdge(3, 4, 30);
+        graph.addEdge(3,5, 20);
+
     }
 
     @Test
     public void kruskalTest() {
-        Graph graph = new Graph();
-        graph.kruskal();
+        List<Edge> mst = graph.kruskal();
+        System.out.println("Minimalne drzewo rozpinające (MST) uzyskane za pomocą algorytmu Kruskala:");
+        for (Edge edge : mst) {
+            System.out.println(edge.v1.id + " - " + edge.v2.id + " : " + edge.weight);
+        }
+    }
+    @Test
+    public void primTest() {
+        System.out.println("Algorytm Prima:");
+
+        List<Edge> mst = graph.prim(1);
+        int totalWeight = 0;
+
+        for (Edge edge : mst) {
+            System.out.println("Krawędź: " + edge.v1.id + " <-> " + edge.v2.id + ", Waga: " + edge.weight);
+            totalWeight += edge.weight;
+        }
+
+        System.out.println("Suma wag MST: " + totalWeight);
+    }
+    @Test
+    public void coloringTest() {
+        // Kolorowanie grafu
+        int numColors = graph.greedyColoring();
+
+        // Wyświetlanie wyniku
+        System.out.println("Minimalna liczba kolorów potrzebnych do pokolorowania grafu: " + numColors);
     }
 }
